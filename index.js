2f25d865565fbfc187c9d5b1c4150384b3f53f25
Andrei <dre021088@gmail.com>
2024-08-22 13:46:04 +0700

    test

--- index.js
+++ index.js
@@ -6,56 +6,52 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-try {
-  const TelegramBot = require("node-telegram-bot-api");
+const TelegramBot = require("node-telegram-bot-api");
 
-  const token = process.env.BOT_TOKEN;
-  const webAppUrl = process.env.WEB_APP_URL;
+const token = process.env.BOT_TOKEN;
+const webAppUrl = process.env.WEB_APP_URL;
 
-  const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
 
-  bot.on("poll", () => {
-    console.log("poll");
-  });
+bot.on("poll", () => {
+  console.log("poll");
+});
 
-  bot.on("polling_error", (error) => {
-    console.log("---polling_error");
-    console.log(error.code);
-    console.log(error.message);
-  });
-  bot.on("error", (error) => {
-    console.log("---error");
-    console.log(error.code);
-    console.log(error.message);
-  });
+bot.on("polling_error", (error) => {
+  console.log("---polling_error");
+  console.log(error.code);
+  console.log(error.message);
+});
+bot.on("error", (error) => {
+  console.log("---error");
+  console.log(error.code);
+  console.log(error.message);
+});
 
-  bot.on("message", async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
 
-    console.log("userMessage:", text);
+  console.log("userMessage:", text);
 
-    if (text === "/start") {
-      await bot.sendMessage(chatId, {
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "Открыть Гороскоп", web_app: { url: webAppUrl } }],
-          ],
-        },
-      });
-    } else {
-      await bot.sendMessage(chatId, "Message recieved");
-    }
-  });
+  if (text === "/start") {
+    await bot.sendMessage(chatId, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Открыть Гороскоп", web_app: { url: webAppUrl } }],
+        ],
+      },
+    });
+  } else {
+    await bot.sendMessage(chatId, "Message recieved");
+  }
+});
 
-  const PORT = 8000;
-  app.listen(PORT, () => {
-    console.log(`Server has been started at port ${PORT}...`);
-  });
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server has been started at port ${PORT}...`);
+});
 
-  setTimeout(() => {
-    console.log(`AAAAAAAAAAAAAAAAAAAAAAA...`);
-  }, 5000);
-} catch (e) {
-  console.error(e);
-}
+setTimeout(() => {
+  console.log(`AAAAAAAAAAAAAAAAAAAAAAA...`);
+}, 10000);
